6027a3d3bb6e4ab785e33b0e2bb7ff4129d1e579
Ryabichin Alexander <ryabichinsasha2013by@gmail.com>
2023-03-05 22:34:07 +0300

    example008.1

--- example008_Dog/Program.cs
+++ example008_Dog/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int dogCounter = 0;
+int Distance = 1000;
+int FirstFriendSpeed = 4;
+int SecondFriendSpeed = 5;
+int DogSpeed = 10;
+int DistanceLimit = 10;
+int DogDerection = 1;
+
+while (Distance > DistanceLimit);
+speed=0;
+
+    if
+    {
+      (DogDerection == 1);
+       Speed = firstFriendSpeed + DogSpeed;
+      (DogDerection = 2) ;
+    }
+    else
+     {
+       Speed = SecondFriendSpeed + DogSpeed;
+       DogDerection = 1;
+       TimeToMeet = (Distance / Speed);
+       Distance = distance - TimeToMeet * (FirstFriendSpeed + SecondFriendSpeed);
+       DogCounter = DogCounter + 1;
+     }       
+Console.Write ("distance =" + distance);
+Console.WriteLine ("DogCounter =" + DogCounter);
